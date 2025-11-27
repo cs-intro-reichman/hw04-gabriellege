1d33daafea6a310fab5b73bc310b0826efdddc98
Gabrielle Geffen <gabrielle.geffen@post.runi.ac.il>
2025-11-27 22:03:57 +0200

    Completed assignment

--- MyString.java
+++ MyString.java
@@ -7,43 +7,37 @@ public class MyString {
         System.out.println("lowercase : " + lowerCase("lowercase"));
 
         System.out.println("Testing contains:");
-        System.out.println(contains("unhappy", "happy"));      // true
-        System.out.println(contains("happy", "unhappy"));      // false
-        System.out.println(contains("historical", "story"));   // false
-        System.out.println(contains("psychology", "psycho"));  // true
-        System.out.println(contains("personality", "son"));    // true
-        System.out.println(contains("personality", "dad"));    // false
-        System.out.println(contains("resignation", "sign"));   // true
+        System.out.println(contains("unhappy", "happy"));
+        System.out.println(contains("happy", "unhappy"));
+        System.out.println(contains("historical", "story"));
+        System.out.println(contains("psychology", "psycho"));
+        System.out.println(contains("personality", "son"));
+        System.out.println(contains("personality", "dad"));
+        System.out.println(contains("resignation", "sign"));
     }
 
     public static String lowerCase(String str) {
-        char[] chars = str.toCharArray();
-        for (int i = 0; i < chars.length; i++) {
-            if (chars[i] >= 'A' && chars[i] <= 'Z') {
-                chars[i] = (char) (chars[i] + 32);
+        char[] arr = str.toCharArray();
+        for (int i = 0; i < arr.length; i++) {
+            if (arr[i] >= 'A' && arr[i] <= 'Z') {
+                arr[i] = (char)(arr[i] + 32);
             }
         }
-        return new String(chars);
+        return new String(arr);
     }
 
     public static boolean contains(String str1, String str2) {
         str1 = lowerCase(str1);
         str2 = lowerCase(str2);
-        int n = str1.length();
-        int m = str2.length();
 
-        if (m == 0) return true;
-        if (m > n) return false;
-
-        for (int i = 0; i <= n - m; i++) {
-            boolean match = true;
-            for (int j = 0; j < m; j++) {
-                if (str1.charAt(i + j) != str2.charAt(j)) {
-                    match = false;
-                    break;
-                }
+        for (int i = 0; i <= str1.length() - str2.length(); i++) {
+            int j = 0;
+            while (j < str2.length() && str1.charAt(i + j) == str2.charAt(j)) {
+                j++;
             }
-            if (match) return true;
+            if (j == str2.length()) {
+                return true;
+            }
         }
         return false;
     }
